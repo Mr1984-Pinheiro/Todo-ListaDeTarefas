5f24a312ea77ad900f5797d0ca76c218bfd01d4f
CARLOS EDUARDO TAVARES PINHEIRO <73333067+Mr1984-Pinheiro@users.noreply.github.com>
2021-11-03 04:31:39 -0300

    Inicia projeto e cria um Header

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Area, Container, Header } from "./App.styles";
+
+const App = () => {
+  return (
+    <Container>
+      <Area>
+        <Header>Lista de Tarefas</Header>
+      </Area>
+    </Container>
+  );
+};
+
+export default App;
